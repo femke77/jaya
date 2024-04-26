77f7bdc43bd2d46c0fc182d9c8b170f2f46c46f6
femke77 <megan.meyers.388@gmail.com>
2024-04-26 02:19:28 -0700

    fixing performance issues"

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
+  build :{
+    minify: "terser"
+  },
   plugins: [
     react(),
     VitePWA({
