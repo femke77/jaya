69ac571a98ee15c9b5c30a4f9cade71d63be4e9f
femke77 <megan.meyers.388@gmail.com>
2024-04-26 01:01:10 -0700

    init

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-a9fe0588'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.pu9kpiumlc"
+    "revision": "0.dupu54snn88"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
